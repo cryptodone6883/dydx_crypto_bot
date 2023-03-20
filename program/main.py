7ba59c0b56064408d223841b347e898d69609eff
cryptodone6883 <113867827+cryptodone6883@users.noreply.github.com>
2023-03-19 23:46:26 -0400

    add function to place or abort market orders

--- program/main.py
+++ program/main.py
@@ -1,4 +1,24 @@
-
+from func_connections import connect_dydx
+from func_private import abort_all_positions
+from constants import ABORT_ALL_POSITIONS
 
 if __name__ == "__main__":
-    print("hello")
+    
+    # connect to client
+    try:
+        print("Connecting to Client...")
+        client = connect_dydx()
+    except Exception as e:
+        #print(e)
+        print("Error connecting to client: ", e)
+        exit(1)
+
+    # Abort all open positions
+    if ABORT_ALL_POSITIONS:
+        try:
+            print("Closing all postions...")
+            close_orders = abort_all_positions(client)
+        except Exception as e:
+            print("Error closing all positions: ", e)
+            exit(1)
+
