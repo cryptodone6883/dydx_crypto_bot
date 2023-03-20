fc4db6f103e817c75a7b7f56ebd868c4c9b879c6
cryptodone6883 <113867827+cryptodone6883@users.noreply.github.com>
2023-03-20 12:12:09 -0400

    stage2 start

--- program/func_utils.py
+++ program/func_utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 
 # Format number
 def format_number(curr_num, match_num):
@@ -17,3 +18,51 @@ def format_number(curr_num, match_num):
         return curr_num_string
     else:
         return f"{int(curr_num)}"
+    
+# Format time
+def format_time(timestamp):
+    return timestamp.replace(microsecond=0).isoformat()
+
+
+# Get ISO times
+def get_ISO_times():
+
+    # Get timestamps
+    date_start_0 = datetime.now()
+    date_start_1 = date_start_0 - timedelta(hours=100)
+    date_start_2 = date_start_1 - timedelta(hours=100)
+    date_start_3 = date_start_2 - timedelta(hours=100)
+    date_start_4 = date_start_3 - timedelta(hours=100)       
+    date_start_5 = date_start_4 - timedelta(hours=100)    
+    date_start_6 = date_start_5 - timedelta(hours=100)    
+
+    # Format datetimes
+    times_dict = {
+        "range_1":{
+            "from_iso": format_time(date_start_1),
+            "to_iso": format_time(date_start_0),
+        },
+        "range_2":{
+            "from_iso": format_time(date_start_2),
+            "to_iso": format_time(date_start_1),
+        },
+        "range_3":{
+            "from_iso": format_time(date_start_3),
+            "to_iso": format_time(date_start_2),
+        },
+        "range_4":{
+            "from_iso": format_time(date_start_4),
+            "to_iso": format_time(date_start_3),
+        },
+        "range_5":{
+            "from_iso": format_time(date_start_5),
+            "to_iso": format_time(date_start_4),
+        },
+        "range_6":{
+            "from_iso": format_time(date_start_6),
+            "to_iso": format_time(date_start_5),
+        },   
+    }
+
+    # return results
+    return times_dict
--- program/main.py
+++ program/main.py
@@ -1,6 +1,7 @@
 from func_connections import connect_dydx
 from func_private import abort_all_positions
-from constants import ABORT_ALL_POSITIONS
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
+from func_public import construct_market_prices
 
 if __name__ == "__main__":
     
@@ -22,3 +23,14 @@ if __name__ == "__main__":
             print("Error closing all positions: ", e)
             exit(1)
 
+
+    # Find cointegrated pairs
+    if FIND_COINTEGRATED:
+        try:
+            print("Fetching market prices, please allow 3 mins ...")
+            df_market_prices = construct_market_prices(client)
+        except Exception as e:
+            print("Error closing all positions: ", e)
+            exit(1)
+
+    
